49f445f7321dc662896b63dec820e3dc0d639568
ktochow1 <ktochow1@binghamton.edu>
2019-01-02 17:09:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,11 +4,12 @@ require 'nokogiri'
 
 
 def create_project_hash
-  projects = {}
   
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
+  
+    projects = {}
   # binding.pry
   
   
