949d66558df5f1cc43c920374112ad52b141bebe
ktochow1 <ktochow1@binghamton.edu>
2019-01-02 16:44:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,16 @@
-# require libraries/modules here
+#require libraries/modules here
+require 'pry'
+require 'nokogiri'
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+
+create_project_hash
+
+# projects: kickstarter.css("li.project.grid_4pro")
