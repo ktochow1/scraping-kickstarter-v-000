ba18b807b94e0cd11935e4fcef0334445eb83e40
ktochow1 <ktochow1@binghamton.edu>
2019-01-02 16:52:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,4 +14,4 @@ end
 create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4pro")
-title: project.css("h2.bbcard_name strong a").text
+#title: project.css("h2.bbcard_name strong a").text
